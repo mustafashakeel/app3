71a9c3c319c587ef505c9f2ad4b2b3d456f51d24
Mustafa Qureshi <mustafashakeel@gmail.com>
2022-02-15 12:07:23 -0800

    hello

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello word
+          Hello word - abc
         </a>
       </header>
     </div>
